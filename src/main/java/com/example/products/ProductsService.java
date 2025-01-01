74533b7f3a8765ea3756071ecbb04a0670a1ae68
Krithika Ravishankar <168807306+Kritzr@users.noreply.github.com>
2025-01-01 22:51:35 +0530

    ProductsService.java

--- src/main/java/com/example/products/ProductsService.java
+++ src/main/java/com/example/products/ProductsService.java
@@ -3,10 +3,14 @@ import com.example.entities.Entities.Products;
 import com.example.entities.Entities.Provisions;
 import com.example.entities.Repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductsService {
@@ -27,9 +31,14 @@ public class ProductsService {
 
         return products;
     }
-    public Provisions updateProvisionDetails(Long provId, Provisions updateProvision) {
+    
+    public List<Products> getProductsByCategory(String category){
+        List<Products> allProducts = productsRepository.findAll();
+        return allProducts.stream().filter(product->product.getPrCategory().equals(category)).collect(Collectors.toList());
+    }
+    public Provisions updateProvisionDetails(Long id, Provisions updateProvision) {
         try{
-            Optional<Provisions> provisionOptional = provisionsRepository.findById(provId);
+            Optional<Provisions> provisionOptional = provisionsRepository.findById(id);
             if (provisionOptional.isPresent()) {
                 Provisions provision = provisionOptional.get();
                 if (updateProvision.getProvName() != null) {
@@ -54,5 +63,25 @@ public class ProductsService {
             throw new RuntimeException("Failed to update the provision details: " +e.getMessage());
         }
 
+    }
+
+    public boolean deleteProvisionById(Long provId) {
+        try {
+            if (provisionsRepository.existsById(provId)) {
+                provisionsRepository.deleteById(provId);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to delete provision: " + e.getMessage());
+        }
+    }
+
+    public List<Products> getLowStockProducts(int threshold) {
+        List<Products> allProducts = productsRepository.findAll();
+        return allProducts.stream()
+                .filter(product -> product.getPrCurrentQuantity() < threshold) // Filter products with low stock
+                .collect(Collectors.toList());
     }
 }
