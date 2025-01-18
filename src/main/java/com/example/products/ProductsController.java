93b64c708c4ad7375ff0d0311bb5facb7aadd291
Kritzr <kritstudy15@gmail.com>
2025-01-18 17:10:20 +0530

    Updated upon checking

--- src/main/java/com/example/products/ProductsController.java
+++ src/main/java/com/example/products/ProductsController.java
@@ -1,8 +1,39 @@
 package com.example.products;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.entities.Entities.Provisions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
+@RequestMapping("/products")
 public class ProductsController {
-    
+
+    @Autowired
+    private ProductsService productsService;
+
+    @PutMapping("/provisions/{id}")
+    public ResponseEntity<?> updateProvision(@PathVariable Long provId, @RequestBody Provisions provision, @RequestHeader(value = "Auth_token", required = true)String authToken){
+        try{
+            if(authToken ==null||authToken.isEmpty()){
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message","Unauthorised: missing or invalid"));
+            }
+            Provisions updateProvision = productsService.updateProvisionDetails(provId, provision);
+            if(updateProvision != null){
+                return ResponseEntity.ok(Map.of("message","provisions updated successfully"));
+            }
+            else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", "provisions not found with id: "+ provId));
+            }
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", "an error had occured :"+ e.getMessage()));
+        }
+
+    }
+
+
 }
